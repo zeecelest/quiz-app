2a75d1a9405a66ecae2f986e19149fe786d772b5
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-20 16:30:07 -0600

    userScore count and question count added to JS. Some pseudocode added

--- index.js
+++ index.js
@@ -8,9 +8,52 @@ const STORE = [
   { id: cuid(), question: 'Fifth question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
 ]
 
+/* initializing userScore and questionNumber to be 0 so that 
+we can userScore++ and questionNumber++ as our app  runs.
+*/
+
+let userScore = 5;
+let questionCount = 8;
+
+//if the user presses the Retake Quiz Button, these functions will
+//be called within the renderStartPage to set the UserScore back to 0.
+// and to set the questionCount back to 0.
+function resetUserScore(){
+  userScore = 0;
+  console.log(userScore);
+}
+
+function resetQuestionCount(){
+  questionCount= 0;
+  console.log(questionCount);
+}
+
+/*renderStartPage should run automatically when the page is loaded.
+It will use the .html() method to push a string of html content to 
+the <main> section in the html. This will allow for the start page
+to be displayed by the user. It should not contain any score values 
+and should reset the question count. Of note, this function will also
+be called by the renderNewGame function when the user hits the "restart 
+quiz button"*/
 
 function renderStartPage(){
   console.log('renderStartPage works');
+  resetUserScore();
+  resetQuestionCount();
+  $('main').html(`
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  `)
 }
 
 
