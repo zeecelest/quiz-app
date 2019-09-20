e9c01f30b00aa0b4ca5f693df2ac213f934714e3
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-20 12:45:28 -0600

    basic html wireframe

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 'use strict';
 
-const questionBank = [
-  { id: cuid(), question: 'First question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
+const STORE = [
+  { id: cuid(), question: 'First question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' }, //answers can be an array of answers
   { id: cuid(), question: 'Second question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
   { id: cuid(), question: 'Third question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
   { id: cuid(), question: 'Fourth question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
   { id: cuid(), question: 'Fifth question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
-  { id: cuid(), question: 'Sixth question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
-  { id: cuid(), question: 'Seventh question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
-  { id: cuid(), question: 'Eighth question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
-  { id: cuid(), question: 'Ninth question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
-  { id: cuid(), question: 'Tenth question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
 ]
