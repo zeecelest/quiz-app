a8c03ed1e85c7b3598422db2e200f97359fc934e
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-20 10:56:25 -0600

    data modelling draft

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const questionBank = [
+  { id: cuid(), question: 'First question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
+  { id: cuid(), question: 'Second question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
+  { id: cuid(), question: 'Third question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
+  { id: cuid(), question: 'Fourth question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
+  { id: cuid(), question: 'Fifth question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
+  { id: cuid(), question: 'Sixth question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
+  { id: cuid(), question: 'Seventh question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
+  { id: cuid(), question: 'Eighth question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
+  { id: cuid(), question: 'Ninth question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
+  { id: cuid(), question: 'Tenth question goes here', answers: 'Answer choices  go here', correct: 'correct answer goes here' },
+]
